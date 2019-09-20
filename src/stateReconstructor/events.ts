bed2905af917416bedf3937ab1a45d1c0110e584
Travis Stodter <travis.stodter@sharpnotions.com>
2019-09-20 19:54:13 -0400

    initial State reconstructor

---
+++ src/stateReconstructor/events.ts
@@ -0,0 +1,33 @@
+import { UnionType } from "./unionHelpers";
+import { Direction } from "./dungeonState";
+
+export const makeFactory = <T extends UnionType>(kind: T['kind']) => (init: Partial<T>): T => ({
+  ...init,
+  kind
+} as T);
+
+export type CommonEvent = {
+  playerId: number;
+};
+
+export type Move = {
+  kind: 'move';
+  direction: Direction;
+} & CommonEvent;
+
+export const Move = makeFactory<Move>('move');
+
+export type PickUp = {
+  kind: 'pick-up';
+  itemId: number;
+} & CommonEvent;
+
+export const PickUp = makeFactory<PickUp>('pick-up');
+
+export type Stab = {
+  kind: 'stab';
+} & CommonEvent;
+
+export const Stab = makeFactory<Stab>('stab');
+
+export type DungeonEvent = Move | PickUp | Stab;
