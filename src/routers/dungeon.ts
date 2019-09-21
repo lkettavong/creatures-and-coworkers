b3bdcfaa2bd91ca752958f742735bac290fbfbc4
Matt Thomson <mthomson.lee@gmail.com>
2019-09-20 21:08:39 -0400

    Look at things and move around

--- src/routers/dungeon.ts
+++ src/routers/dungeon.ts
@@ -2,19 +2,30 @@ import Router from 'koa-router';
 import ForsakeGoblinTemplate from '../templates/ForsakenGoblin.json';
 import { reduceState } from '../stateReconstructor';
 import { Move, PickUp, Stab } from '../stateReconstructor/events';
+import { DungeonState } from '../stateReconstructor/dungeonState';
 const router = new Router();
 router.prefix('/dungeon');
 
+const forsakenGoblinTemple = DungeonState(ForsakeGoblinTemplate);
+const findPlayer = (dungeonState: DungeonState, id: number) => dungeonState.players.filter(player => player.id === id)[0];
+const viewRoom = (dungeonState: DungeonState, playerRoom: string) => dungeonState.rooms.filter(room => room.roomName === playerRoom)[0];
+
 router.get('/', async ctx => {
-  //const { db } = ctx;
-  //const { rows: results } = await db.raw('select * from users;');
   ctx.body = ForsakeGoblinTemplate;
 });
 
+router.get('/look', async ctx => {
+  const playerRoom = findPlayer(forsakenGoblinTemple, 1).room;
+  ctx.body = viewRoom(forsakenGoblinTemple, playerRoom);
+});
+
 router.get('/move/:direction', async ctx => {
-  ctx.body = reduceState(ForsakeGoblinTemplate)(Move({
+  const updatedState = reduceState(forsakenGoblinTemple)(Move({
     playerId: 1, direction: ctx.params.direction
   }));
+  const playerRoom = findPlayer(updatedState, 1).room;
+  forsakenGoblinTemple.players[0].room = playerRoom;
+  ctx.body = viewRoom(forsakenGoblinTemple, playerRoom);
 });
 
 export default router;
